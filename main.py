cd839c021e81c94cb73765243e5491f92b52d61e
Natthakit Iewprasert <nonusae@gmail.com>
2025-04-06 15:40:30 +0700

    Add clock amd tick to game

--- main.py
+++ main.py
@@ -4,13 +4,22 @@ def main():
     print("Starting Asteroids!")
     print(f"Screen width: {SCREEN_WIDTH}")
     print(f"Screen height: {SCREEN_HEIGHT}")
+
+    # Setup display and clock
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+    clock = pygame.time.Clock()
+    dt = 0
+
+    # Main game loop
     while True:
         screen.fill((0, 0, 0))
         pygame.display.flip()
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
+
+        t = clock.tick(60)
+        dt = t / 1000
 
 if __name__ == "__main__":
     main()
